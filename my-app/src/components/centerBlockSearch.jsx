513dfd449d0cf40d95272968bdd73cf22268713a
Arina Gracheva <arinee.msc@gmail.com>
2023-08-21 23:57:14 +0300

    components

---
+++ my-app/src/components/centerBlockSearch.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./centerBlockSearch.css";
+function search() {
+    return (
+    <div className="centerblock__search search">
+              <svg className="search__svg">
+                <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
+              </svg>
+              <input
+                className="search__text"
+                type="search"
+                placeholder="Поиск"
+                name="search"
+              />
+            </div>
+    );
+}
+
+export default search;
---
+++ my-app/src/components/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+
+function footer() {
+    return (
+        <footer className="footer"></footer>
+    );
+}
+
+export default footer;
