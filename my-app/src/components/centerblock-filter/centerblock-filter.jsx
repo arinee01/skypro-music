369723bc2bfc01857e74334468396e789d18bc03
Arina Gracheva <arinee.msc@gmail.com>
2023-11-09 11:54:22 +0300

    filtr

--- my-app/src/components/centerblock-filter/centerblock-filter.jsx
+++ my-app/src/components/centerblock-filter/centerblock-filter.jsx
@@ -1,52 +1,94 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import * as S from './centerblock.styles'
+import { useGetAllTracksQuery } from '../../services/playlists'
+import { useDispatch, useSelector } from 'react-redux'
+import { filtersSelector } from '../../store/selectors/selectors'
+import { setFilteredPlaylist, setFilters } from '../../store/slices/trackSlice'
 
 const PerformerListFilter = () => {
+  const { data: playlist } = useGetAllTracksQuery()
+  
+  const authors = playlist?.map((track) => track.author)
+  const authorsSet = new Set(authors)
+  const allAuthors = Array.from(authorsSet)
+
+  const dispatch = useDispatch()
+  const filters = useSelector(filtersSelector)
+
+  const filterAuthor = (authorFilter) => {
+    dispatch(setFilters({...filters, status: true, authors: authorFilter}))
+  }
+  
+  const removeFilter = () => {
+    dispatch(setFilters({...filters, status: false, authors: ''}))
+  }
+
+  const toggleAuthorFilter = (filter) => {
+    if (!filters.status) {
+      filterAuthor(filter)
+    } else {
+      removeFilter()
+    }
+    console.log(filter)
+  }
+
   return (
     <S.FilterScroll>
       <S.FilterTextListUl>
-        <S.FilterText>Nero</S.FilterText>
-        <S.FilterText>Dynoro, Outwork, Mr. Gee</S.FilterText>
-        <S.FilterText>Ali Backgor</S.FilterText>
-        <S.FilterText>Стоункат, Psychopath</S.FilterText>
-        <S.FilterText>Jaded, Will Clarke, AR/CO</S.FilterText>
-        <S.FilterText>Blue Fountain, Zeds Dead</S.FilterText>
-        <S.FilterText>
-          HYBIT, Mr. Black, Offer Nissim, Hi Profile
-        </S.FilterText>
-        <S.FilterText>minthaze</S.FilterText>
-        <S.FilterText>Calvin Harris, Disciples</S.FilterText>
-        <S.FilterText>Tom Boxer</S.FilterText>
+        {allAuthors?.map((author, index) => (
+          <S.FilterText
+            key={`author-${index}`}
+            onClick={() => toggleAuthorFilter(author)}
+          >
+            {author}
+          </S.FilterText>
+        ))}
       </S.FilterTextListUl>
     </S.FilterScroll>
   )
 }
 
 const YearListFilter = () => {
+  const dispatch = useDispatch()
+  const filters = useSelector(filtersSelector)
+  
+  const filterYears = (yearFilter) => {
+    dispatch(setFilters({ ...filters, status: true, years: yearFilter }))
+  }
+
   return (
     <S.FilterScroll>
       <S.FilterTextListUl>
-        <S.FilterText>По умолчанию</S.FilterText>
-        <S.FilterText>Сначала новые</S.FilterText>
-        <S.FilterText>Сначала старые</S.FilterText>
+        <S.FilterText onClick={() => filterYears('По умолчанию')}>
+          По умолчанию
+        </S.FilterText>
+        <S.FilterText onClick={() => filterYears('Сначала новые')}>
+          Сначала новые
+        </S.FilterText>
+        <S.FilterText onClick={() => filterYears('Сначала старые')}>
+          Сначала старые
+        </S.FilterText>
       </S.FilterTextListUl>
     </S.FilterScroll>
   )
 }
 
 const GenreListFilter = () => {
+  const dispatch = useDispatch()
+  const filters = useSelector(filtersSelector)
+
+  const filterGenre = (genreFilter) => {
+    dispatch(setFilters({ ...filters, status: true, genre: genreFilter }))
+  }
+
   return (
     <S.FilterScroll>
       <S.FilterTextListUl>
-        <S.FilterText>Хип-хоп</S.FilterText>
-        <S.FilterText>Поп-музыка</S.FilterText>
-        <S.FilterText>Техно</S.FilterText>
-        <S.FilterText>Инди</S.FilterText>
-        <S.FilterText>Рок-музыка</S.FilterText>
-        <S.FilterText>Кантри</S.FilterText>
-        <S.FilterText>Джаз</S.FilterText>
-        <S.FilterText>Классическая</S.FilterText>
+        <S.FilterText onClick={() => filterGenre(false)}>По умолчанию</S.FilterText>
+        <S.FilterText onClick={() => filterGenre('Классическая музыка')}>Классическая музыка</S.FilterText>
+        <S.FilterText onClick={() => filterGenre('Электронная музыка')}>Электронная музыка</S.FilterText>
+        <S.FilterText onClick={() => filterGenre('Рок музыка')}>Рок музыка</S.FilterText>
       </S.FilterTextListUl>
     </S.FilterScroll>
   )
@@ -56,6 +98,16 @@ export function CenterBlockFilter() {
   const [performerFilter, setPerformerFilter] = useState(false)
   const [yearFilter, setYearFilter] = useState(false)
   const [genreFilter, setGenreFilter] = useState(false)
+
+  //
+  const filters = useSelector(filtersSelector)
+  const { data: playlist } = useGetAllTracksQuery()
+  const [authorsFilter, setAuthorsFilter] = useState('')
+
+  useEffect(() => {
+    setAuthorsFilter(playlist?.map((track) => track.author))
+  }, [playlist])
+  //
 
   const togglePerformerCategory = () => {
     setPerformerFilter((prev) => !prev)
